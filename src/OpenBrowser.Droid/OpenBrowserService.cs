03bff4f01fdb9e784fe4cc4b2dbbe5348359b1f0
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-02-10 13:45:58 +1100

    Update OpenBrowserService.cs

--- src/OpenBrowser.Droid/OpenBrowserService.cs
+++ src/OpenBrowser.Droid/OpenBrowserService.cs
@@ -10,15 +10,16 @@ namespace OpenBrowser.Droid
     /// <summary>
     /// Opens the supplied Uri in the default browser.
     /// </summary>
-    public class OpenBrowserService : Activity, IOpenBrowserService
+    public class OpenBrowserService : IntentService, IOpenBrowserService
+    {
+    	// TODO: Fix lifecycle - https://developer.xamarin.com/guides/android/application_fundamentals/services/part_1_-_started_services/#Implementing_IntentService.OnHandleIntent
+        public Task OpenDefaultBrowser(System.Uri uri)
 	{
-		public Task OpenDefaultBrowser(System.Uri uri)
-		{
-			var intent = new Intent (Intent.ActionView, Android.Net.Uri.Parse(uri.ToString()));
-		    StartActivity(intent);
+            var intent = new Intent (Intent.ActionView, Android.Net.Uri.Parse(uri.ToString()));
+    	    StartActivity(intent);
 
             return Task.FromResult(true);
-        }
+	}
     }
 }
 
